aaa7d6d5a10f81e2a637a668cbdca81cd8b74011
Nicolas Depetri <Nicolas@Server.server>
2020-03-17 23:22:08 -0300

    CommitInitial
    CommitInitial

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,8 +42,8 @@
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
+import 'core-js/es/reflect';
 import '@angular/localize/init';
 
 /** ALL Firefox browsers require the following to support**/ `@angular/animation`
